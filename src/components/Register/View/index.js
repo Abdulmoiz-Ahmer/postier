e9c210b2387ed2db7bc4496dc2b36641b825c56b
Abdulmoiz <abdulmoizahmer1996@gmail.com>
2022-01-04 13:55:33 -0700

    ADDS: Heading for register

--- src/components/Register/View/index.js
+++ src/components/Register/View/index.js
@@ -1,5 +1,6 @@
 import React from "react";
+import { Header } from "semantic-ui-react";
 
 export const View = function View() {
-  return <div>Register</div>;
+  return <Header as="h1">Register</Header>;
 };
