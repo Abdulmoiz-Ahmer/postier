2822948577f95dfd7df17dd30448863373e1cc87
Maheen Komal <maheenkomal992@gmail.com>
2022-01-05 21:45:42 +0500

    mock design

--- src/components/Register/View/index.js
+++ src/components/Register/View/index.js
@@ -1,6 +1,34 @@
 import React from "react";
-import { Header } from "semantic-ui-react";
+import { Grid, Image, Button, Checkbox, Form } from "semantic-ui-react";
 
 export const View = function View() {
-  return <Header as="h1">Register</Header>;
+  return (
+    <Grid textAlign="center" verticalAlign="middle" container>
+      <Grid.Row>
+        <Grid.Column width={6} textAlign="center">
+          <Image src="https://media.istockphoto.com/vectors/hand-holds-letter-and-inserts-it-into-mailbox-vector-id1128685907?k=20&m=1128685907&s=612x612&w=0&h=YBLFz7Ai3zxSyewqnQSBY0XRUtqtNYnqcbiXGhSadpA=" />
+        </Grid.Column>
+
+        <Grid.Column color="orange" width={10} textAlign="center">
+          <Form>
+            <Form.Field>
+              <label>Full Name</label>
+              <input placeholder="Full Name" />
+            </Form.Field>
+            <Form.Input label="PostBoxNumber" placeholder="PostBoxNumber" />
+            <Form.Input label="Password" placeholder="Enter Password" />
+            <Form.Input
+              label="Confirm Password"
+              placeholder="Confirm Password"
+            />
+            <Form.Input label="Email" placeholder="joe@schmoe.com" />
+            <Form.Field>
+              <Checkbox label="I agree to the Terms and Conditions" />
+            </Form.Field>
+            <Button type="submit">Submit</Button>
+          </Form>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
 };
