4d7c785c3cb5d79bc11cd54e8eddfa541af4b966
Abdulmoiz <abdulmoizahmer1996@gmail.com>
2022-01-05 19:23:21 -0700

    ADDS: login page

--- src/components/Login/LoginViewProvider/index.js
+++ src/components/Login/LoginViewProvider/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import { Model } from "../Model";
 import { ViewModel } from "../ViewModel";
 import { ViewController } from "../ViewController";
+import { useTranslation } from "react-i18next";
+import { transformToCapitalized } from "../../../utils";
 
-export const LoginViewProvider = function index() {
-  return <ViewController />;
+export const LoginViewProvider = function () {
+  const { t } = useTranslation();
+  return (
+    <ViewController t={t} transformToCapitalized={transformToCapitalized} />
+  );
 };
