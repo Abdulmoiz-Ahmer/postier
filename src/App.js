d385494bc046f13ec00ee5717647e7511b726727
Abdulmoiz <abdulmoizahmer1996@gmail.com>
2022-01-09 12:06:59 -0700

    ADDS: route changed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import { BrowserRouter } from "react-router-dom";
-// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Layout } from "./elements";
-// import { Login } from "./pages";
-// import { Register } from "./pages";
-// import { PostBox } from "./pages";
-// import { Letter } from "./pages";
-// import { ErrorPage } from "./pages";
 import { store } from "./store";
 import { Provider } from "react-redux";
 import { AppRoutes } from "./app-routes";
 import "./App.css";
-// import { Auth } from "./elements/Auth";
 
 function App() {
   return (
--- src/elements/Auth/index.js
+++ src/elements/Auth/index.js
@@ -2,5 +2,5 @@ import { useAuth } from "../../hooks/useAuth";
 import { Navigate } from "react-router-dom";
 
 export const Auth = function ({ children }) {
-  return useAuth() === true ? children : <Navigate to="/" replace />;
+  return useAuth() === true ? children : <Navigate to="/login" replace />;
 };
